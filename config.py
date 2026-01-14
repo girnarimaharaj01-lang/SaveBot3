6da98021ad9b139c15e41a4ded83d88370eb26d5
girnarimaharaj01-lang <girnarimaharaj01@gmail.com>
2026-01-14 15:02:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ BOT_TOKEN = getenv("BOT_TOKEN", "8346695576:AAErSTkMH1Jcb-yK0nE2gKCuSorGg0OhQMI"
 OWNER_ID = list(map(int, getenv("OWNER_ID", "8399557684").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://sujay5372192:sujay5372192@cluster00001.zivqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster00001")
 LOG_GROUP = getenv("LOG_GROUP", "-1003164986113")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002483720229"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1003164986113"))
