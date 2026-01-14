bec2764dcee677743acac70c92010ce556b1ed7a
girnarimaharaj01-lang <girnarimaharaj01@gmail.com>
2026-01-14 14:58:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", "24935727"))
-API_HASH = getenv("API_HASH", "3fd33336629324ecd664e9b6894f0909")
-BOT_TOKEN = getenv("BOT_TOKEN", "7609700133:AAG6TqHDZt3Mdi3Za5XKaP-xfcu0HFjwxTA")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "7336971189").split()))
+API_ID = int(getenv("API_ID", "29777466"))
+API_HASH = getenv("API_HASH", "a04b3df726520026f207079aec2f9879")
+BOT_TOKEN = getenv("BOT_TOKEN", "8346695576:AAErSTkMH1Jcb-yK0nE2gKCuSorGg0OhQMI")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "8399557684").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://sujay5372192:sujay5372192@cluster00001.zivqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster00001")
-LOG_GROUP = getenv("LOG_GROUP", "-1002262545667")
+LOG_GROUP = getenv("LOG_GROUP", "-1003164986113")
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002483720229"))
